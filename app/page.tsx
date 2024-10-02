89621b05c11d6ebdf58a50981346f58eee361615
Damian <37555910+DCRepublic@users.noreply.github.com>
2024-10-02 12:03:40 -0400

    add search on main page and prisma schema v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
 "use client";
 
+import { Input } from "@nextui-org/react";
+import SearchIcon from "@mui/icons-material/Search";
+
 import { CourseBig } from "@/components/course-big";
+
 export default function Home() {
   return (
-    <div className="grid gap-4 max-h-8/12 overflow-scroll w-7/12">
-      <CourseBig color="bg-red-500" />
-      <CourseBig color="bg-yellow-500" />
-      <CourseBig color="bg-green-500" />
+    <div className=" h-[550px] grid grid-cols-1 gap-4 -mt-10">
+      <Input
+        classNames={{
+          base: "max-w-full sm:max-w-[10rem] h-12 mt-0",
+          mainWrapper: "h-full",
+          input: "text-small",
+          inputWrapper:
+            "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+        }}
+        placeholder="Type to search..."
+        size="sm"
+        startContent={<SearchIcon />}
+        type="search"
+      />
+
+      <div className="grid gap-4  overflow-scroll w-7/12">
+        <CourseBig color="bg-red-500" />
+        <CourseBig color="bg-yellow-500" />
+        <CourseBig color="bg-green-500" />
+        <CourseBig color="bg-green-500" />
+      </div>
     </div>
   );
 }
