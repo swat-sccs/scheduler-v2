ff276f9da071b32ff3b97fa636e9d90764d42a74
Damian <37555910+DCRepublic@users.noreply.github.com>
2024-11-10 21:09:43 -0500

    add rating average to profs

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -5,7 +5,7 @@ export default function DocsLayout({
 }) {
   return (
     <section className="flex flex-col items-center justify-center gap-4 max-h-[75vh] -mt-5 ">
-      <div className="inline-block max-w-lg text-center justify-center overflow-scroll rounded-lg scrollbar-hide">
+      <div className="inline-block  text-center justify-center ">
         {children}
       </div>
     </section>
