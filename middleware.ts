fa33cc08d4734ca5395d51606c292330c892baca
Thomas Makin <halorocker89@gmail.com>
2024-11-21 15:48:11 -0500

    meta: more updates

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 
+export const config = { matcher: ["/", "/calendar", "/rating"] };
+
 export default withAuth({
   // Matches the pages config in `[...nextauth]`
   pages: {
